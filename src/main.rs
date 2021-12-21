7a9f0dfd84332e79b26f577904813e2980958637
arbrog <arbrog@gmail.com>
2021-12-21 05:05:08 +0000

    common inputs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use fast_poisson::{Poisson2D, PoissonVariable2D};
 
 fn main() {
+    let dim = 2.0;
+    let r = 1.0;
+    let k = 30;
+    let seed = 123123;
     let points = Poisson2D::new()
-        .with_dimensions([500.0, 500.0], 32.0)
-        .with_samples(30)
+        .with_dimensions([dim, dim], r)
+        .with_seed(seed)
+        .with_samples(k)
         .generate();
     println!("num of points {:?}", points.len());
 
     let points = PoissonVariable2D::new()
-        .with_dimensions([10.0, 10.0], 2.0)
-        .with_seed(123123)
-        .with_samples(30)
+        .with_dimensions([dim, dim], r)
+        .with_seed(seed)
+        .with_samples(k)
         .generate();
     println!("num of points {:?}", points.len());
 }
